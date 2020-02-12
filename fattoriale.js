78d8c4444ec65c02c762859ff44ad4bebc572f15
Mirko1807 <59826817+Mirko1807@users.noreply.github.com>
2020-02-12 19:09:02 +0100

    Update fattoriale.js

--- fattoriale.js
+++ fattoriale.js
@@ -1,3 +1,25 @@
 /**
 * Scrivere qui il codice della funzione fattoriale(n)
 **/
+let q = require('readline')
+let y = q.createInterface({
+    input: process.stdin,
+    output: process.stdout
+
+})
+
+y.question('di quale numero vuoi il fattoriale? ', (fatt) => {
+    let fattoriale = x => {
+        let r = x
+        while (x > 1) {
+            r = r * (x - 1)
+            x = x - 1
+        }
+        return r
+
+    }
+    let rt = fattoriale(fatt)
+    console.log(rt)
+    y.close()
+
+})
